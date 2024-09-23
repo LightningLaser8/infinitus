14d5661dd5a9c9a0f638e0e3cdbf874514dd281c
LightningLaser8 <LightningLaser8@users.noreply.github.com>
2024-09-23 20:02:18 +0100

    Add progress so far

---
+++ scripts/outpost.js
@@ -0,0 +1,42 @@
+const outpost = extend(StorageBlock, "0c-1-01-outpost", {});
+
+outpost.buildType = () =>
+  extend(StorageBlock.StorageBuild, outpost, {
+    displayReact(table) {
+      this.super$displayReact(table);
+    },
+    craft(a, b) {},
+    acceptItem(source, item) {
+      const { items, team } = this;
+      const { building: core } = team.core();
+      return core
+        ? acceptItem(source, item)
+        : items.get(item) < this.getMaximumAccepted(item);
+    },
+    removeStack(item, amount) {
+      const { team } = this;
+      const core = team.core();
+      const result = this.super$removeStack(item, amount);
+      if (core == null) return;
+      const { state } = Vars;
+      const { defaultTeam, sector } = state.rules;
+      if (core && team == defaultTeam && state.isCampaign())
+        sector.info.handleCoreItem(item, -result);
+      return result;
+    },
+    handleItem(source, item) {
+      const core = this.team.core();
+      if (core) {
+        const { items, storageCapacity } = core;
+        if (items.get(item) >= storageCapacity)
+          this.block.incinerateEffect(this, source);
+        core.handleItem(source, item);
+        return;
+      }
+      this.super$handleItem(source, item);
+    },
+    itemTaken(item) {
+      const core = this.team.core();
+      if (core) core.itemTaken(item);
+    },
+  });
